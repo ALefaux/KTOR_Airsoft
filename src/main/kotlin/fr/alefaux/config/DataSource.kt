8b926f0182808e993dda86a1a2724faa522855dd
Axel Lefaux <axel.lefaux@outlook.fr>
2021-12-19 21:54:07 +0100

    Change db url

--- src/main/kotlin/fr/alefaux/config/DataSource.kt
+++ src/main/kotlin/fr/alefaux/config/DataSource.kt
@@ -9,14 +9,10 @@ object DataSource {
     fun devDataSource(): HikariDataSource {
         val appConfig = HoconApplicationConfig(ConfigFactory.load())
         val dbUrl = appConfig.property("ktor.db.uri").getString()
-        val dbUser = appConfig.property("ktor.db.username").getString()
-        val dbPassword = appConfig.property("ktor.db.password").getString()
 
         val config = HikariConfig().apply {
             driverClassName = "org.postgresql.Driver"
             jdbcUrl = dbUrl
-            username = dbUser
-            password = dbPassword
             validate()
         }
         return HikariDataSource(config)
