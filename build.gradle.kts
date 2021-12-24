ebe35594b3f75879627bbff3cd6ae289846145b3
Axel Lefaux <axel.lefaux@outlook.fr>
2021-12-24 10:15:36 +0100

    Remove graphql to use routing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
-val kgraphql_version: String by project
+val graphql_version: String by project
 val koin_version: String by project
-val kmongo_version: String by project
 
 plugins {
     application
@@ -38,18 +37,13 @@ tasks.create("stage") {
 
 dependencies {
     implementation("io.insert-koin:koin-ktor:$koin_version")
-    implementation("org.litote.kmongo:kmongo:$kmongo_version")
     implementation("io.ktor:ktor-server-core:$ktor_version")
     implementation("io.ktor:ktor-gson:$ktor_version")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("com.apurebase:kgraphql:$kgraphql_version")
-    implementation("com.apurebase:kgraphql-ktor:$kgraphql_version")
-    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.6.0")
-    
+
     // Exposed & Postgresql
     implementation("org.jetbrains.exposed:exposed:0.17.14")
-    implementation("com.zaxxer:HikariCP:4.0.3")
+    implementation("com.zaxxer:HikariCP:4.0.3") // Do not pass to version 5 cause not support jvm 1.8
     implementation("org.postgresql:postgresql:42.3.1")
 }
